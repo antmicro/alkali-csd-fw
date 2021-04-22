1dd162a4931bbac3b17927923b1778fe26f7c285
Piotr Binkowski <pbinkowski@antmicro.com>
2021-04-22 16:00:27 +0200

    rpu-app: Add handling RDY flag

--- rpu-app/src/main.c
+++ rpu-app/src/main.c
@@ -8,20 +8,52 @@
 #include <sys/printk.h>
 #include <irq.h>
 
+#include "nvme_tc.h"
+
+enum State {IDLE = 0, RDY = 1};
+
+typedef struct nvme_tc_priv {
+	mem_addr_t base;
+	enum State state;
+} nvme_tc_priv_t;
+
+nvme_tc_priv_t priv = {0};
+
 void nvme_irq_handler(void *arg)
 {
 	printk("NVMe IRQ\n");
+
+	while(sys_read32(priv.base + NVME_TC_IRQ_STA_REG)) {
+		uint16_t reg = sys_read32(priv.base + NVME_TC_IRQ_DAT_REG) * 4;
+		if(reg == NVME_TC_CC_REG) {
+			uint32_t cc = sys_read32(priv.base + reg);
+			if (cc & NVME_TC_CC_EN)
+				sys_write32(1, priv.base + NVME_TC_CSTS_REG);
+		}
+		printk("Host write to reg 0x%04x\n", reg);
+	}
 }
 
-void nvme_irq_init()
+void nvme_irq_init(void)
 {
-	IRQ_CONNECT(DT_INST_0_NVME_TC_IRQ_0, DT_INST_0_NVME_TC_IRQ_0_PRIORITY, nvme_irq_handler, 0, DT_INST_0_NVME_TC_IRQ_0_FLAGS);
+	printk("Configuring interrupts: %d\n", DT_INST_0_NVME_TC_IRQ_0);
+	IRQ_CONNECT(DT_INST_0_NVME_TC_IRQ_0, DT_INST_0_NVME_TC_IRQ_0_PRIORITY, nvme_irq_handler, &priv, DT_INST_0_NVME_TC_IRQ_0_FLAGS);
 	irq_enable(DT_INST_0_NVME_TC_IRQ_0);
+	printk("Interrupts configured\n");
+}
+
+void nvme_tc_init(void)
+{
+	priv.base = (mem_addr_t)DT_INST_0_NVME_TC_BASE_ADDRESS;
+	priv.state = IDLE;
 }
 
 void main(void)
 {
 	printk("NVMe Controller FW for %s\n", CONFIG_BOARD);
+
+	nvme_tc_init();
 	nvme_irq_init();
+
 	while(1);
 }
---
+++ rpu-app/src/nvme_tc.h
@@ -0,0 +1,12 @@
+#ifndef NVME_TC_H
+#define NVME_TC_H
+
+#define NVME_TC_CC_REG	0x14
+#define NVME_TC_CSTS_REG 0x1c
+
+#define NVME_TC_CC_EN	0x1
+
+#define NVME_TC_IRQ_STA_REG	0x1028
+#define NVME_TC_IRQ_DAT_REG	0x102c
+
+#endif
