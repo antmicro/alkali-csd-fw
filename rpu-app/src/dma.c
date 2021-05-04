5b7245598da68bd6bbafd8fbcd3875a501696d9b
Piotr Binkowski <pbinkowski@antmicro.com>
2021-05-04 11:52:05 +0200

    rpu-app: dma: Add support for transfer pending transfer queues

--- rpu-app/src/dma.c
+++ rpu-app/src/dma.c
@@ -12,6 +12,9 @@ typedef struct nvme_dma_priv {
 	struct k_fifo tx_fifo;
 	struct k_fifo rx_fifo;
 
+	bool tx_dma_running;
+	bool rx_dma_running;
+
 	struct k_mem_slab desc_slab;
 } nvme_dma_priv_t;
 
@@ -29,9 +32,27 @@ nvme_dma_priv_t p_dma = {0};
 
 char __aligned(16) desc_slab_buffer[sizeof(nvme_dma_xfer_def_t)*NVME_DMA_SLAB_ENTRIES];
 
+static void nvme_dma_setup_xfer(nvme_dma_priv_t *priv, nvme_dma_xfer_def_t *desc, uint32_t off)
+{
+	printk("Configuring DMA transfer\n");
+
+	sys_write32(desc->host_addr & 0xffffffff, priv->base + off + NVME_DMA_REG_PCIE_ADDRL);
+	sys_write32((desc->host_addr >> 32) & 0xffffffff, priv->base + off + NVME_DMA_REG_PCIE_ADDRH);
+
+	// We use 32-bit AXI addresses, upper 32-bits are ignored
+	sys_write32(desc->local_addr, priv->base + off + NVME_DMA_REG_AXI_ADDRL);
+	sys_write32(0, priv->base + off + NVME_DMA_REG_AXI_ADDRH);
+
+	sys_write32(desc->len, priv->base + off + NVME_DMA_REG_LEN);
+
+	// Writing tag starts transfer
+	sys_write32(desc->tag, priv->base + off + NVME_DMA_REG_TAG);
+}
+
 void nvme_dma_irq_handler(void *arg)
 {
 	nvme_dma_priv_t *priv = (nvme_dma_priv_t*)arg;
+	unsigned long long lock;
 
 	uint32_t read_status = sys_read32(priv->base + NVME_DMA_REG_STATUS + NVME_DMA_REG_READ_BASE);
 	uint32_t write_status = sys_read32(priv->base + NVME_DMA_REG_STATUS + NVME_DMA_REG_WRITE_BASE);
@@ -45,6 +66,17 @@ void nvme_dma_irq_handler(void *arg)
 		} else {
 			printk("Spurious DMA RX interrupt!\n");
 		}
+
+		lock = irq_lock();
+
+		// Check if there is another transfer pending
+		desc = k_fifo_peek_head(&priv->rx_fifo);
+		if(desc)
+			nvme_dma_setup_xfer(priv, desc, NVME_DMA_REG_READ_BASE);
+		else
+			priv->rx_dma_running = false;
+
+		irq_unlock(lock);
 	}
 
 	if(write_status & NVME_DMA_REG_STATUS_VALID) {
@@ -56,6 +88,17 @@ void nvme_dma_irq_handler(void *arg)
 		} else {
 			printk("Spurious DMA TX interrupt!\n");
 		}
+
+		lock = irq_lock();
+
+		// Check if there is another transfer pending
+		desc = k_fifo_peek_head(&priv->tx_fifo);
+		if(desc)
+			nvme_dma_setup_xfer(priv, desc, NVME_DMA_REG_WRITE_BASE);
+		else
+			priv->tx_dma_running = false;
+
+		irq_unlock(lock);
 	}
 }
 
@@ -111,31 +154,20 @@ static nvme_dma_xfer_def_t *nvme_dma_xfer_fill_desc(nvme_dma_priv_t *priv, uint6
 	return desc;
 }
 
-static void nvme_dma_setup_xfer(nvme_dma_priv_t *priv, nvme_dma_xfer_def_t *desc, uint32_t off)
-{
-	printk("Configuring DMA transfer\n");
-
-	sys_write32(desc->host_addr & 0xffffffff, priv->base + off + NVME_DMA_REG_PCIE_ADDRL);
-	sys_write32((desc->host_addr >> 32) & 0xffffffff, priv->base + off + NVME_DMA_REG_PCIE_ADDRH);
-
-	// We use 32-bit AXI addresses, upper 32-bits are ignored
-	sys_write32(desc->local_addr, priv->base + off + NVME_DMA_REG_AXI_ADDRL);
-	sys_write32(0, priv->base + off + NVME_DMA_REG_AXI_ADDRH);
-
-	sys_write32(desc->len, priv->base + off + NVME_DMA_REG_LEN);
-
-	// Writing tag starts transfer
-	sys_write32(desc->tag, priv->base + off + NVME_DMA_REG_TAG);
-}
-
 int nvme_dma_xfer_host_to_mem(void* arg, uint64_t src, uint32_t dst, uint32_t len, nvme_dma_xfer_cb *cb, void *cb_arg)
 {
 	nvme_dma_priv_t *priv = (nvme_dma_priv_t*)arg;
 	nvme_dma_xfer_def_t *desc = nvme_dma_xfer_fill_desc(priv, src, dst, len, 0xAA, cb, cb_arg);
+	unsigned long long lock;
 
 	if(desc) {
+		lock = irq_lock();
 		k_fifo_put(&priv->rx_fifo, desc);
-		nvme_dma_setup_xfer(priv, desc, NVME_DMA_REG_READ_BASE);
+		if(!priv->rx_dma_running) {
+			nvme_dma_setup_xfer(priv, desc, NVME_DMA_REG_READ_BASE);
+			priv->rx_dma_running = true;
+		}
+		irq_unlock(lock);
 		return 0;
 	} else {
 		return -ENOMEM;
@@ -146,10 +178,16 @@ int nvme_dma_xfer_mem_to_host(void* arg, uint32_t src, uint64_t dst, uint32_t le
 {
 	nvme_dma_priv_t *priv = (nvme_dma_priv_t*)arg;
 	nvme_dma_xfer_def_t *desc = nvme_dma_xfer_fill_desc(priv, dst, src, len, 0x55, cb, cb_arg);
+	unsigned long long lock;
 
 	if(desc) {
+		lock = irq_lock();
 		k_fifo_put(&priv->tx_fifo, desc);
-		nvme_dma_setup_xfer(priv, desc, NVME_DMA_REG_WRITE_BASE);
+		if(!priv->tx_dma_running) {
+			nvme_dma_setup_xfer(priv, desc, NVME_DMA_REG_WRITE_BASE);
+			priv->tx_dma_running = true;
+		}
+		irq_unlock(lock);
 		return 0;
 	} else {
 		return -ENOMEM;
