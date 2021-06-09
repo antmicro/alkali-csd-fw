041157946c8cff212d700462a9fe1e2b7f360a6f
Piotr Binkowski <pbinkowski@antmicro.com>
2021-06-09 17:59:43 +0200

    rpu-app: add script for RPU gdb debugging

---
+++ rpu-app/rpu.tcl
@@ -0,0 +1,17 @@
+connect -url TCP:localhost:6000
+
+source /opt/Xilinx/Vitis/2019.2/scripts/vitis/util/zynqmp_utils.tcl
+
+targets -set -nocase -filter {name =~ "RPU*"}
+
+enable_split_mode
+
+targets -set -nocase -filter {name =~ "APU*"}
+
+set mode [expr [mrd -value 0xff5e0200] & 0xf]
+
+targets -set -nocase -filter {name =~ "*R5*#0"}
+
+rst -processor
+
+dow build/zephyr/zephyr.elf
