c68fee635def7fede009b41e60215f47bb04646f
Piotr Binkowski <pbinkowski@antmicro.com>
2021-12-21 15:42:00 +0100

    apu-app: Cleanup TFLite wrappers

--- apu-app/src/vm.h
+++ apu-app/src/vm.h
@@ -8,9 +8,7 @@ extern "C" {
 void register_functions(struct ubpf_vm *vm);
 
 void vm_print(char *buf);
-void vm_tflite(char *ibuf, char *obuf, int isize, int osize);
-void vm_tflite_float(char *ibuf, char *obuf, int isize, int osize, int model_size);
-void vm_tflite_uint(char *ibuf, char *obuf, int isize, int osize, int model_size);
+void vm_tflite_apu(char *ibuf, char *obuf, int isize, int osize, int model_size);
 void vm_tflite_vta(char *ibuf, char *obuf, int isize, int osize, int model_size);
 
 #endif
--- apu-app/src/vm/tf.cpp
+++ apu-app/src/vm/tf.cpp
@@ -13,185 +13,71 @@
 
 #include <time.h>
 
-void vm_tflite(char *ibuf, char *obuf, int isize, int osize)
-{
-	static const char model_path[] = "/bin/model.tflite";
+#define DEBUG
 
-	std::unique_ptr<tflite::FlatBufferModel> model = tflite::FlatBufferModel::BuildFromFile(model_path);
+static void tflite_handler(char *model_buf, char *input_buf, char *output_buf, int model_len, int input_len, int output_len, bool with_vta)
+{
+	struct timespec ts[2];
+
+	std::unique_ptr<tflite::FlatBufferModel> model = tflite::FlatBufferModel::BuildFromBuffer(model_buf, model_len);
 
 	// Build the interpreter
 	tflite::ops::builtin::BuiltinOpResolver resolver;
 	std::unique_ptr<tflite::Interpreter> interpreter;
 	tflite::InterpreterBuilder(*model, resolver)(&interpreter);
 
-	std::unique_ptr<TfLiteDelegate, decltype(&tflite::TfLiteCustomDelegateDelete)> delegate(tflite::TfLiteCustomDelegateCreate(NULL), &tflite::TfLiteCustomDelegateDelete);
-
-	interpreter->ModifyGraphWithDelegate(std::move(delegate));
+	if(with_vta) {
+		std::unique_ptr<TfLiteDelegate, decltype(&tflite::TfLiteCustomDelegateDelete)> delegate(tflite::TfLiteCustomDelegateCreate(NULL), &tflite::TfLiteCustomDelegateDelete);
+		interpreter->ModifyGraphWithDelegate(std::move(delegate));
+	}
 
 	// Resize input tensors, if desired.
 	interpreter->AllocateTensors();
 
-	float* input = interpreter->typed_input_tensor<float>(0);
+	// Check if buffer sizes are correct
+	auto in = interpreter->input_tensor(0);
+	auto out = interpreter->output_tensor(0);
+	if(input_len != in->bytes)
+		printf("Input buffer length mismatch: %d != %d\n", input_len, in->bytes);
+	if(output_len != out->bytes)
+		printf("Output buffer length mismatch: %d != %d\n", output_len, out->bytes);
+
+	void *input_tensor = (void*)interpreter->typed_input_tensor<int8_t>(0);
+	if(!input_tensor)
+		input_tensor = (void*)interpreter->typed_input_tensor<float>(0);
+
 	// ... pass input data to the input array
+	std::copy(input_buf, input_buf+input_len, (char*)input_tensor);
 
-	std::copy(ibuf, ibuf+isize, input);
-
-	struct timespec ts[2];
 	timespec_get(&ts[0], TIME_UTC);
-
-	// run model
 	interpreter->Invoke();
-
 	timespec_get(&ts[1], TIME_UTC);
 
+#ifdef DEBUG
 	const uint64_t duration = (ts[1].tv_sec * 1000000000 + ts[1].tv_nsec) - (ts[0].tv_sec * 1000000000 + ts[0].tv_nsec);
-
 	printf("Model processing took %llu ns\n", duration);
+#endif
 
 	// get pointer to outputs
-	float* output = interpreter->typed_output_tensor<float>(0);
+	void *output_tensor = interpreter->typed_output_tensor<int8_t>(0);
+	if(!output_tensor)
+		output_tensor = interpreter->typed_output_tensor<float>(0);
 
-	std::copy((char*)output, ((char*)output) + osize, obuf);
-
-	printf("%f %f\n", output[0], output[1]);
+	std::copy((char*)output_tensor, ((char*)output_tensor) + output_len, output_buf);
 }
 
-void vm_tflite_float(char *ibuf, char *obuf, int isize, int osize, int model_size)
+void vm_tflite_apu(char *ibuf, char *obuf, int isize, int osize, int model_size)
 {
-	const char *model_buf = ibuf;
-	ibuf += model_size;
+	char *model_buf = ibuf;
+	char *input_buf = ibuf+model_size;
 
-	std::unique_ptr<tflite::FlatBufferModel> model = tflite::FlatBufferModel::BuildFromBuffer(model_buf, model_size);
-
-	// Build the interpreter
-	tflite::ops::builtin::BuiltinOpResolver resolver;
-	std::unique_ptr<tflite::Interpreter> interpreter;
-	tflite::InterpreterBuilder(*model, resolver)(&interpreter);
-
-	std::unique_ptr<TfLiteDelegate, decltype(&tflite::TfLiteCustomDelegateDelete)> delegate(tflite::TfLiteCustomDelegateCreate(NULL), &tflite::TfLiteCustomDelegateDelete);
-
-	interpreter->ModifyGraphWithDelegate(std::move(delegate));
-
-	// Resize input tensors, if desired.
-	interpreter->AllocateTensors();
-
-	float* input = interpreter->typed_input_tensor<float>(0);
-	// ... pass input data to the input array
-
-	std::copy(ibuf, ibuf+isize, input);
-
-	struct timespec ts[2];
-	timespec_get(&ts[0], TIME_UTC);
-
-	// run model
-	interpreter->Invoke();
-
-	timespec_get(&ts[1], TIME_UTC);
-
-	const uint64_t duration = (ts[1].tv_sec * 1000000000 + ts[1].tv_nsec) - (ts[0].tv_sec * 1000000000 + ts[0].tv_nsec);
-
-	printf("Model processing took %llu ns\n", duration);
-
-	// get pointer to outputs
-	float* output = interpreter->typed_output_tensor<float>(0);
-
-	std::copy((char*)output, ((char*)output) + osize, obuf);
-
-	for(int i = 0; i < 1000; i++) {
-		if (i%20 == 0)
-			printf("\n");
-		printf("%f ", output[i]);
-	}
-}
-
-void vm_tflite_uint(char *ibuf, char *obuf, int isize, int osize, int model_size)
-{
-	const char *model_buf = ibuf;
-	ibuf += model_size;
-
-	std::unique_ptr<tflite::FlatBufferModel> model = tflite::FlatBufferModel::BuildFromBuffer(model_buf, model_size);
-
-	// Build the interpreter
-	tflite::ops::builtin::BuiltinOpResolver resolver;
-	std::unique_ptr<tflite::Interpreter> interpreter;
-	tflite::InterpreterBuilder(*model, resolver)(&interpreter);
-
-	// Resize input tensors, if desired.
-	interpreter->AllocateTensors();
-
-	int8_t* input = interpreter->typed_input_tensor<int8_t>(0);
-	// ... pass input data to the input array
-
-	std::copy(ibuf, ibuf+isize, input);
-
-	struct timespec ts[2];
-	timespec_get(&ts[0], TIME_UTC);
-
-	// run model
-	interpreter->Invoke();
-
-	timespec_get(&ts[1], TIME_UTC);
-
-	const uint64_t duration = (ts[1].tv_sec * 1000000000 + ts[1].tv_nsec) - (ts[0].tv_sec * 1000000000 + ts[0].tv_nsec);
-
-	printf("Model processing took %llu ns\n", duration);
-
-	// get pointer to outputs
-	int8_t* output = interpreter->typed_output_tensor<int8_t>(0);
-
-	std::copy((char*)output, ((char*)output) + osize, obuf);
-
-	for(int i = 0; i < 1000; i++) {
-		if (i%20 == 0)
-			printf("\n");
-		printf("%d ", output[i]);
-	}
+	tflite_handler(model_buf, input_buf, obuf, model_size, isize, osize, false);
 }
 
 void vm_tflite_vta(char *ibuf, char *obuf, int isize, int osize, int model_size)
 {
-	const char *model_buf = ibuf;
-	ibuf += model_size;
+	char *model_buf = ibuf;
+	char *input_buf = ibuf+model_size;
 
-	std::unique_ptr<tflite::FlatBufferModel> model = tflite::FlatBufferModel::BuildFromBuffer(model_buf, model_size);
-
-	// Build the interpreter
-	tflite::ops::builtin::BuiltinOpResolver resolver;
-	std::unique_ptr<tflite::Interpreter> interpreter;
-	tflite::InterpreterBuilder(*model, resolver)(&interpreter);
-
-	std::unique_ptr<TfLiteDelegate, decltype(&tflite::TfLiteCustomDelegateDelete)> delegate(tflite::TfLiteCustomDelegateCreate(NULL), &tflite::TfLiteCustomDelegateDelete);
-
-	interpreter->ModifyGraphWithDelegate(std::move(delegate));
-
-	// Resize input tensors, if desired.
-	interpreter->AllocateTensors();
-
-	int8_t * input = interpreter->typed_input_tensor<int8_t>(0);
-	// ... pass input data to the input array
-
-	std::copy(ibuf, ibuf+isize, input);
-
-	struct timespec ts[2];
-	timespec_get(&ts[0], TIME_UTC);
-
-	// run model
-	interpreter->Invoke();
-
-	timespec_get(&ts[1], TIME_UTC);
-
-	const uint64_t duration = (ts[1].tv_sec * 1000000000 + ts[1].tv_nsec) - (ts[0].tv_sec * 1000000000 + ts[0].tv_nsec);
-
-	printf("Model processing took %llu ns\n", duration);
-
-	// get pointer to outputs
-	int8_t* output = interpreter->typed_output_tensor<int8_t>(0);
-
-	std::copy((char*)output, ((char*)output) + osize, obuf);
-
-	for(int i = 0; i < 1000; i++) {
-		if (i%20 == 0)
-			printf("\n");
-		printf("%d ", output[i]);
-	}
+	tflite_handler(model_buf, input_buf, obuf, model_size, isize, osize, true);
 }
