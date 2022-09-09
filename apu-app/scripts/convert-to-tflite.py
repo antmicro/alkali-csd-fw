ae99b5ae508cf30768dbab017b5ad6e59f35acb2
Grzegorz Latosinski <glatosinski@antmicro.com>
2022-09-09 18:41:41 +0200

    apu-app: Added missing license headers
    Internal-tag: [#37217]

--- apu-app/scripts/convert-to-tflite.py
+++ apu-app/scripts/convert-to-tflite.py
@@ -1,3 +1,8 @@
+# Copyright 2021-2022 Western Digital Corporation or its affiliates
+# Copyright 2021-2022 Antmicro
+#
+# SPDX-License-Identifier: Apache-2.0
+
 import tensorflow as tf
 import numpy as np
 from tqdm import tqdm
--- apu-app/tests/tests-main.cpp
+++ apu-app/tests/tests-main.cpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2021-2022 Western Digital Corporation or its affiliates
+ * Copyright 2021-2022 Antmicro
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include <gtest/gtest.h>
 
 int main(int argc, char *argv[])
