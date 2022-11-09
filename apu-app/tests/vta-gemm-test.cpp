c50fd9150f0eab89f151c8e17a146a7d874a8059
Grzegorz Latosinski <glatosinski@antmicro.com>
2022-11-09 20:32:27 +0100

    apu-app: tests: vta-gemm-test: Added test for padding
    Internal-tag: [#38726]
    
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- apu-app/tests/vta-gemm-test.cpp
+++ apu-app/tests/vta-gemm-test.cpp
@@ -37,20 +37,6 @@ TEST(VTAGEMM, permuteDimsTest)
 
     op.permuteDims(inputlayout, outputlayout, input.data(), output.data(), 1);
 
-    spdlog::info("\n\n");
-    spdlog::info("inp:  ");
-    for (int i = 0; i < numelements; i++)
-    {
-        spdlog::info("{} ", input[i]);
-    }
-    spdlog::info("\n\n");
-    spdlog::info("out:  ");
-    for (int i = 0; i < numelements; i++)
-    {
-        spdlog::info("{} ", output[i]);
-    }
-    spdlog::info("\n\n");
-
     for (int No = 0; No < op.dim("No"); No++)
         for (int Ni = 0; Ni < op.dim("Ni"); Ni++)
             for (int Io = 0; Io < op.dim("Io"); Io++)
@@ -78,83 +64,42 @@ TEST(VTAGEMM, permuteDimsTest)
                         }
 }
 
-TEST(VTAGEMM, permuteDimsTestWithPadding)
+TEST(VTAGEMM, padDataTestSimple)
 {
     tflite::VTAGEMMOp op(nullptr, nullptr, kTfLiteBuiltinConv2d, {0, 1, 2}, {3});
-    op.setDim("No", 1);
-    op.setDim("Ni", 4);
-    op.setDim("Io", 4);
-    op.setDim("Ii", 4);
-    op.setDim("H", 4);
-    op.setDim("W", 4);
+    op.setDim("Ni", 1);
+    op.setDim("Hi", 2);
+    op.setDim("Wi", 3);
+    op.setDim("Ci", 1);
 
-    // actual dimensions
-    op.setDim("Ni'", 3);
-    op.setDim("Ii'", 2);
+    op.setDim("No", 2);
+    op.setDim("Ho", 2);
+    op.setDim("Wo", 3);
+    op.setDim("Co", 2);
 
-    std::vector<std::string> inputlayout =  {"No", "Ni", "Io", "Ii", "H", "W"};
-    std::vector<std::string> outputlayout = {"No", "Io", "H", "W", "Ni", "Ii"};
-    std::vector<std::string> actuallayout =  {"No", "Ni'", "Io", "Ii'", "H", "W"};
+    std::vector<std::string> inputlayout = {"Ni", "Hi", "Wi", "Ci"};
+    std::vector<std::string> outputlayout = {"No", "Ho", "Wo", "Co"};
 
-    int inpnumelements = op.tensorElements(actuallayout);
-    int outnumelements = op.tensorElements(outputlayout);
+    std::vector<uint8_t> input = {
+        1, 2, 3,
+        4, 5, 6
+    };
 
-    std::vector<uint8_t> input(inpnumelements);
-    std::vector<uint8_t> output(outnumelements);
+    std::vector<uint8_t> actualoutput = {
+        1, 0, 2, 0, 3, 0,
+        4, 0, 5, 0, 6, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0
+    };
 
-    int i = 0;
-    std::transform(input.cbegin(), input.cend(), input.begin(), [&i](uint8_t val){ return i++; });
+    std::vector<uint8_t> output(op.tensorElements(outputlayout));
 
-    op.permuteDims(inputlayout, outputlayout, input.data(), output.data(), 1, &actuallayout);
+    op.padData(inputlayout, outputlayout, input.data(), output.data(), 1);
 
-    spdlog::info("\n\n");
-    spdlog::info("inp:  ");
-    for (int i = 0; i < inpnumelements; i++)
+    for (unsigned int i = 0; i < actualoutput.size(); i++)
     {
-        spdlog::info("{} ", input[i]);
+        ASSERT_EQ(actualoutput[i], output[i])
+            << i << "th element differs (actual: " << actualoutput[i]
+            << " computed: " <<output[i];
     }
-    spdlog::info("\n\n");
-    spdlog::info("out:  ");
-    for (int i = 0; i < outnumelements; i++)
-    {
-        spdlog::info("{} ", output[i]);
-    }
-    spdlog::info("\n\n");
-
-    for (int No = 0; No < op.dim("No"); No++)
-        for (int Ni = 0; Ni < op.dim("Ni"); Ni++)
-            for (int Io = 0; Io < op.dim("Io"); Io++)
-                for (int Ii = 0; Ii < op.dim("Ii"); Ii++)
-                    for (int H = 0; H < op.dim("H"); H++)
-                        for (int W = 0; W < op.dim("W"); W++)
-                        {
-                            uint8_t valoutput = output[
-                                No * op.getDimStep(outputlayout, "No") +
-                                Ni * op.getDimStep(outputlayout, "Ni") +
-                                Io * op.getDimStep(outputlayout, "Io") +
-                                Ii * op.getDimStep(outputlayout, "Ii") +
-                                H * op.getDimStep(outputlayout, "H") +
-                                W * op.getDimStep(outputlayout, "W")
-                            ];
-                            if (Ii >= op.dim("Ii'"))
-                            {
-                                ASSERT_EQ(0, valoutput);
-                            }
-                            else if (Ni >= op.dim("Ni'"))
-                            {
-                                ASSERT_EQ(0, valoutput);
-                            }
-                            else
-                            {
-                                uint8_t valinput = input[
-                                    No * op.getDimStep(inputlayout, "No") +
-                                    Ni * op.getDimStep(inputlayout, "Ni") +
-                                    Io * op.getDimStep(inputlayout, "Io") +
-                                    Ii * op.getDimStep(inputlayout, "Ii") +
-                                    H * op.getDimStep(inputlayout, "H") +
-                                    W * op.getDimStep(inputlayout, "W")
-                                ];
-                                ASSERT_EQ(valinput, valoutput);
-                            }
-                        }
 }
